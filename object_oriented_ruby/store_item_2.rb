c3f4346584fb205839fc69c162ee181d4a143565
Paul McCarthy <paul.mccarthy@avant.com>
2017-11-06 21:22:44 -0600

    allow for multiple types

--- object_oriented_ruby/store_item_2.rb
+++ object_oriented_ruby/store_item_2.rb
@@ -2,23 +2,25 @@ class Pokemon
   attr_reader :name, :species, :types
   attr_accessor :level
 
-  def initialize(name, species, types, level)
-    @name = name
-    @species = species
-    @types = types
-    @level = level
+  def initialize(input_hash)
+    @name = input_hash[:name]
+    @species = input_hash[:species]
+    @type1 = input_hash[:type1]
+    @type2 = input_hash[:type2]
+    @level = input_hash[:level]
   end
 
   def display_id
-    puts "This is level #{@level} #{@species}. It goes by #{@name}"
+    puts "This is level #{@level} #{@species}. It is a #{@type1}#{'/' if @type2 != nil}#{@type2} pokemon. It goes by #{@name}"
   end
 end
 
-bulbasaur = Pokemon.new("Bulby", "Bulbasaur", [:grass, :poison], 5)
+bulbasaur = Pokemon.new({name: "Bulby", species: "Bulbasaur", type1: :grass, type2: :poison, level: 5})
+charmander = Pokemon.new({name: "Charmy", species: "Charmander", type1: :fire, level: 5})
 
 bulbasaur.display_id
 puts bulbasaur.name
 bulbasaur.level += 1
-bulbasaur.display_id
+charmander.display_id
 
 puts "test"
