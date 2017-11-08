18158dc537528145862540740bfdd74701cd96a6
Paul McCarthy <paul.mccarthy@avant.com>
2017-11-07 23:09:59 -0600

    11/7 exercise

--- object_oriented_ruby/exercise.rb
+++ object_oriented_ruby/exercise.rb
@@ -7,3 +7,13 @@ end
 
 p double_numbers([4, 1, 3])  #=> "[8, 2, 6]"
 p 'y'
+
+numbers = [1, 2, 4, 2]
+sum = 0
+index = 0
+while index < numbers.length
+  number = numbers[index]
+  sum += number
+  index += 1
+end
+p sum
