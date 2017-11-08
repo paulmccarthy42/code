64caba029e95324fc9c85a7483c0644b0e05b8e8
Paul McCarthy <paul.mccarthy@avant.com>
2017-11-07 19:10:46 -0600

    New folder

--- each_loop.rb
+++ each_loop.rb
@@ -1,18 +1,16 @@
-# Read about the Ruby `each` method for arrays. Then refactor the code below using `each`.
-# ```numbers = [1, 2, 4, 2]
-# sum = 0
-# index = 0
-# numbers.length.times do
-#   number = numbers[index]
-#   sum = sum + number
-#   index = index + 1
-# end
-# p sum```
-# post answer to Peter by starting a thread
+#Read about the Ruby `each` method for arrays. Then refactor the code below using `each`.
+numbers = [1, 2, 4, 2]
+sum = 0
+index = 0
+numbers.length.times do
+  number = numbers[index]
+  sum = sum + number
+  index = index + 1
+end
+p sum
 
 numbers = [1,2,4,2]
 sum = 0
 numbers.each { |x| sum += x}
 puts sum
-
 
