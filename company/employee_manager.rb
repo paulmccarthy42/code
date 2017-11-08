732d503a35ebb889d91cd726177de54870a53971
Paul McCarthy <paul.mccarthy@avant.com>
2017-11-07 20:42:13 -0600

    manual wrapping

--- company/employee_manager.rb
+++ company/employee_manager.rb
@@ -18,8 +18,14 @@ class Employee
   end
 end
 
-employee1 = Employee.new({first_name: "Majora", last_name: "Carter", salary: 80000, active: true})
-employee2 = Employee.new(first_name: "Danilo", last_name: "Campos", salary: 70000, active: true)
+employee1 = Employee.new({first_name: "Majora", 
+  last_name: "Carter", 
+  salary: 80000, 
+  active: true})
+employee2 = Employee.new(first_name: "Danilo", 
+  last_name: "Campos", 
+  salary: 70000, 
+  active: true)
 employee1.print_info
 employee2.print_info
 
@@ -55,7 +61,11 @@ class Manager < Employee
   end
 end
 
-manager = Manager.new(first_name: "Saron", last_name: "Yitbarek", salary: 100000, active: true, employees: [employee1, employee2])
+manager = Manager.new(first_name: "Saron", 
+  last_name: "Yitbarek", 
+  salary: 100000, 
+  active: true, 
+  employees: [employee1, employee2])
 manager.print_info
 manager.send_report
 manager.give_all_raises
