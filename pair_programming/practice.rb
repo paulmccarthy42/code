ec9de40bbf89d60f6d2349328ddba8bba71b4465
Paul McCarthy <paul.mccarthy@avant.com>
2017-11-09 17:58:06 -0600

    pair programming stuff

---
+++ pair_programming/practice.rb
@@ -0,0 +1,33 @@
+# array_of_favorite_foods = []
+# 5.times do
+#   puts "Please input one of your favorite foods:"
+#   food = gets.chomp
+#   array_of_favorite_foods << food
+# end
+
+# number = 1
+# array_of_favorite_foods.each do |food|
+#   puts " #{number}. I love #{food}"
+#   number += 1
+# end
+
+
+true or false
+true
+
+
+
+
+
+recipes = 12
+languages = 6
+crepes = true
+french = true
+
+if recipes > 10 and languages > 5
+  puts "Sam and Sally should date"
+end
+
+if crepes == true and french == true
+  puts "Sam and Sally should marry."
+end
