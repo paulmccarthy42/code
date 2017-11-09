6076ef98d11b7792fe893805e4f7ed35f973f2cc
Paul McCarthy <paul.mccarthy@avant.com>
2017-11-08 18:29:24 -0600

    module example

---
+++ object_oriented_ruby/module_example.rb
@@ -0,0 +1,37 @@
+module Rideable
+  def initialize
+    @speed = 0
+    @direction = 'north'
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+end
+
+class Car
+  include Rideable
+
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
+end
+
+class Bike
+  include Rideable
+
+  def ring_bell
+    puts "Ring ring!"
+  end
+end
+
+car = Car.new
+car.honk_horn
